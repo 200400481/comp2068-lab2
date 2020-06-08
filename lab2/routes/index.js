a8d4705016c9c2086c68178f9baf65f2ff4fbda9
Jakob Clements <jakobclements95@gmail.com>
2020-06-08 13:49:07 -0400

    Add calculate function

--- lab2/routes/index.js
+++ lab2/routes/index.js
@@ -4,6 +4,39 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
+
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y);
+    var method = req.query.method;
+    var result;
+    var operator;
+    var error = false;
+
+    if (method == "add") {
+        operator = "+";
+        result = x + y;
+    }
+    else if (method == "subtract") {
+        operator = "-";
+        result = x - y;
+    }
+    else if (method == "multiply") {
+        operator = "x";
+        result = x * y;
+    }
+    else if (method == "divide") {
+        operator = "/";
+        result = x / y;
+    }
+    else {
+        console.log("Error: Method \'" + method + "'\ is not valid");
+        error = true;
+    }
+
+    if (!error) {
+        console.log(x + " " + operator + " " + y + " = " + result);
+    }
+
     res.render('index', { title: 'Express' });
 });
 
